4cef0d99bdccbd2a4c687c8c7c612faea754f4e7
pavolbincik <pavolbincik@gmail.com>
2015-06-16 16:07:37 +0200

    create environment

---
+++ GruntFile.js
@@ -0,0 +1,84 @@
+module.exports = function (grunt)
+{
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: "\n\n"
+			},
+			dist: {
+				src: ['src/resources/js/**/*.js'],
+				dest: 'src/<%= pkg.name %>.js'
+			},
+			deps: {
+				src: [
+					'bower_components/modernizr/modernizr.js',
+					'bower_components/jquery/dist/jquery.js',
+					'bower_components/bootstrap/dist/js/bootstrap.js',
+					'bower_components/angularjs/angular.min.js',
+					'bower_components/angular-route/angular-route.min.js'
+				],
+				dest: 'src/<%= pkg.name %>-deps.js'
+			},
+			css: {
+				src: ['bower_components/bootstrap/dist/css/bootstrap.min.css',
+						'src/resources/css/styles.css'
+				],
+				dest: 'src/resources/css/<%= pkg.name %>.css'
+			},
+			move: {
+				src: ['bower_components/angularjs/angular.min.js.map'],
+				dest: 'src/angular.min.js.map'
+			}
+		},
+
+        ngAnnotate: {
+            options:{
+                singleQuotes: true
+            },
+            dist:{
+                files:[{
+                    'expand': true,
+                    'src': 'src/<%= pkg.name %>.js',
+                    'ext': '.annotated.js',
+                    'extDot': 'last'
+                }]
+            }
+        },
+
+		less: {
+			dev: {
+				files: {
+					'src/resources/css/styles.css': 'src/resources/css/styles.less'
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['src/resources/js/**/*.js'],
+				tasks: ['concat:dist']
+			},
+			styles: {
+				files: ['src/resources/css/*.less'],
+				tasks: ['less']
+			}
+		}
+	});
+
+	//npm tasks
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-ng-annotate');
+
+	//tasks
+	grunt.registerTask('default', 'Default Task Alias', ['build']);
+
+	grunt.registerTask('build', 'Build the application', 
+		['less:dev',
+		'concat',
+		]);
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dog-diary
 Ever imagine what it would be like if your pets had diaries? What would your favorite animal friend write about their day, about their life, about you?
+
+#Instalation process
+npm install
+bower install
