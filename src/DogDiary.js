a5378dfc363d5738f1b0f42777c35eb5ac1eeccc
Monika <xminkax@gmail.com>
2015-06-17 11:56:00 +0200

    Js files added main structure

--- src/DogDiary.js
+++ src/DogDiary.js
@@ -1,35 +1,77 @@
 /* use strict */
 var app = angular.module('DogDiaryApp', ['ngRoute']);
 
-app.controller('MainCtrl', function($scope){
-   $scope.message = 'palo';
-});
+/* use strict */
+app.controller('AddFoodCtrl', ['$scope', 'dataFoodFactory', function ($scope, dataFoodFactory) {
+        $scope.foods = {};
 
-app.controller('AddMessageCtrl', function($scope){
-    $scope.message = 'palo';
-});
+        $scope.insertFood = function () {
+            //Fake customer data
+            var food = {
+                date: $scope.date
+            };
+            dataFoodFactory.insert(food)
+                .success(function () {
+                    $scope.status = 'Inserted Customer! Refreshing customer list.';
+                    $scope.foods.push(food);
+                }).
+                error(function (error) {
+                    $scope.status = 'Unable to insert customer: ' + error.message;
+                });
+        };
+    }]);
 
+
+/* use strict */
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+    }]);
+
+/* use strict */
+app.factory('dataFoodFactory', ['$http', function ($http) {
+        var urlBase = '/api/foods';
+        var dataFactory = {};
+
+        dataFactory.getAll = function () {
+            return $http.get(urlBase);
+        };
+
+        dataFactory.get = function (id) {
+            return $http.get(urlBase + '/' + id);
+        };
+
+        dataFactory.insert = function (food) {
+            return $http.post(urlBase, food);
+        };
+
+        dataFactory.update = function (cust) {
+            return $http.put(urlBase + '/' + cust.ID, cust)
+        };
+
+        dataFactory.delete = function (id) {
+            return $http.delete(urlBase + '/' + id);
+        };
+
+        return dataFactory;
+    }]);
+
+
+/* use strict */
 app.config(function ($routeProvider) {
 
     $routeProvider.when('/', {
         templateUrl: 'partials/home.html',
         controller: "MainCtrl"
     })
-    //.when('/calendar/:month/:day/:year',{
-    //    templateUrl: "partials/calendar.html",
-    //    controller: "CalendarCtrl"
-    //})
-    .when('/addMessage', {
-            templateUrl: 'partials/addMessage.html',
-            controller: "AddMessageCtrl"
+        //.when('/calendar/:month/:day/:year',{
+        //    templateUrl: "partials/calendar.html",
+        //    controller: "CalendarCtrl"
+        //})
+        .when('/addMessage', {
+            templateUrl: 'partials/addFood.html',
+            controller: "AddFoodCtrl"
         })
         .otherwise({
             template: '<h1>404</h1>'
         })
 });
-//
-//app.controller('CalendarCtrl', function($scope, $routeParams){
-//    $scope.model = {
-//        message: "Date: " + $routeParams.month + " / " + $routeParams.day + " / " + $routeParams.year
-//    };
-//})
