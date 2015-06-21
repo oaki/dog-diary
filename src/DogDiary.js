22e633b69a78dc2915d31fa5bfbc60cc8f4cb680
Monika <xminkax@gmail.com>
2015-06-21 10:46:27 +0200

    refactoring chart methods move to chartService

--- src/DogDiary.js
+++ src/DogDiary.js
@@ -151,121 +151,78 @@ app.controller('AddPoopCtrl', ['$scope', 'dataFactory', '$location', 'Upload', f
 
 
 /* use strict */
-app.controller('MainCtrl', ['$scope', 'foodDataFactory', 'poopDataFactory', '$q', function ($scope, foodDataFactory, poopDataFactory, $q) {
-    var ctrl = this;
+app.controller('MainCtrl', [
+    '$scope',
+    'foodDataFactory',
+    'poopDataFactory',
+    'chartService',
+    '$q',
+    function ($scope, foodDataFactory, poopDataFactory, chartService, $q) {
+        var ctrl = this;
 
-    $scope.series = [
-        'Food weight',
-        'Dufalact',
-        'Poop'
-    ];
-
-    ctrl.attributesFood = {
-        0: "weight",
-        1: "dufalact"
-    };
-
-    ctrl.attributesPoop = {
-        2: "size"
-    };
-
-    ctrl.dataMappingFood = {};
-    ctrl.dataMappingPoop = {};
-
-    $scope.$watch('days', function () {
-        $scope.labels = [];
-        $scope.data = [
-            [],
-            [],
-            []
+        $scope.series = [
+            'Food weight',
+            'Dufalact',
+            'Poop'
         ];
 
-        ctrl.generateDayRange($scope.days);
-        ctrl.setValuesToChart(ctrl.dataMappingFood, ctrl.attributesFood);
-        ctrl.setValuesToChart(ctrl.dataMappingPoop, ctrl.attributesPoop);
-    });
+        ctrl.attributesFood = {
+            0: "weight",
+            1: "dufalact"
+        };
 
-    ctrl.dateToYMD = function (date) {
-        var d = date.getDate();
-        var m = date.getMonth() + 1;
-        var y = date.getFullYear();
-        return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-    };
+        ctrl.attributesPoop = {
+            2: "size"
+        };
 
-    ctrl.generateDayRange = function (day) {
-        for (var i = 0; i < day; i++) {
-            var dateObj = new Date();
-            dateObj.setDate(dateObj.getDate() - i);
-            $scope.labels.push(ctrl.dateToYMD(dateObj));
-        }
-    };
+        ctrl.dataMappingFood = {};
+        ctrl.dataMappingPoop = {};
 
-    ctrl.setDataWithDate = function (data) {
-        angular.forEach(data, function (value) {
-            var date = value.datetime.date;
-            if (date !== undefined) {
-                value.datetime.date = date.split(" ")[0];
-            }
-        });
-    };
+        $scope.$watch('days', function () {
+            $scope.labels = [];
+            $scope.data = [
+                [],
+                [],
+                []
+            ];
 
-    ctrl.getMapValuesToDate = function (data, attributes) {
-        var dataMapping = {};
+            $scope.labels = chartService.getDayRange($scope.days);
 
-        angular.forEach(data, function (value) {
-            var date = value.datetime.date;
-            if (dataMapping[date] === undefined) {
-                dataMapping[date] = {};
-                angular.forEach(attributes, function (attribute) {
-                    dataMapping[date][attribute] = value[attribute];
-                });
-            } else {
-                angular.forEach(attributes, function (attribute) {
-                    dataMapping[date][attribute] += value[attribute];
-                });
-            }
+            chartService.setValuesToChart(
+                ctrl.dataMappingFood,
+                ctrl.attributesFood,
+                $scope.labels,
+                $scope.data
+            );
+            chartService.setValuesToChart(
+                ctrl.dataMappingPoop,
+                ctrl.attributesPoop,
+                $scope.labels,
+                $scope.data
+            );
         });
 
-        return dataMapping;
-    };
-
-    ctrl.setValuesToChart = function (dataMapping, attributes) {
-        for (var i = 0; i < $scope.labels.length; i++) {
-            if (typeof dataMapping[$scope.labels[i]] === 'undefined') {
-                angular.forEach(attributes, function (attribute, key) {
-                    $scope.data[key].push(0);
+        ctrl.getData = function () {
+            $q.all(
+                    [
+                        poopDataFactory.getAll(),
+                        foodDataFactory.getAll()
+                    ]
+                ).then(function (response) {
+                    ctrl.dataMappingPoop = chartService.getDataForChart(
+                        response[0].data.poops,
+                        ctrl.attributesPoop
+                    );
+                    ctrl.dataMappingFood = chartService.getDataForChart(
+                        response[1].data.foods,
+                        ctrl.attributesFood
+                    );
+                    $scope.days = 5;
                 });
-            } else {
-                angular.forEach(attributes, function (attribute, key) {
-                    $scope.data[key].push(dataMapping[$scope.labels[i]][attribute]);
-                });
-            }
-        }
-    };
+        };
 
-    ctrl.getDataForChart = function (data, attributes) {
-        //set date to usable format
-        ctrl.setDataWithDate(data);
-
-        //map values to date
-        return ctrl.getMapValuesToDate(data, attributes);
-    };
-
-    ctrl.getData = function () {
-        $q.all(
-                [
-                    poopDataFactory.getAll(),
-                    foodDataFactory.getAll()
-                ]
-            ).then(function (response) {
-                ctrl.dataMappingPoop = ctrl.getDataForChart(response[0].data.poops, ctrl.attributesPoop);
-                ctrl.dataMappingFood = ctrl.getDataForChart(response[1].data.foods, ctrl.attributesFood);
-                $scope.days = 5;
-            });
-    };
-
-    ctrl.getData();
-}]);
+        ctrl.getData();
+    }]);
 
 app.directive('dateTimePicker', function() {
     return {
@@ -371,3 +328,79 @@ app.config(function ($routeProvider) {
             template: '<h1>404</h1>'
         })
 });
+
+/* use strict */
+app.service('chartService', [function () {
+    var service = this;
+
+    service.dateToYMD = function (date) {
+        var d = date.getDate();
+        var m = date.getMonth() + 1;
+        var y = date.getFullYear();
+        return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+    };
+
+    service.getDayRange = function (day) {
+        var labels = [];
+
+        for (var i = 0; i < day; i++) {
+            var dateObj = new Date();
+            dateObj.setDate(dateObj.getDate() - i);
+            labels.push(service.dateToYMD(dateObj));
+        }
+
+        return labels;
+    };
+
+    service.setDataWithDate = function (data) {
+        angular.forEach(data, function (value) {
+            var date = value.datetime.date;
+            if (date !== undefined) {
+                value.datetime.date = date.split(" ")[0];
+            }
+        });
+    };
+
+    service.getMapValuesToDate = function (data, attributes) {
+        var dataMapping = {};
+
+        angular.forEach(data, function (value) {
+            var date = value.datetime.date;
+            if (dataMapping[date] === undefined) {
+                dataMapping[date] = {};
+                angular.forEach(attributes, function (attribute) {
+                    dataMapping[date][attribute] = value[attribute];
+                });
+            } else {
+                angular.forEach(attributes, function (attribute) {
+                    dataMapping[date][attribute] += value[attribute];
+                });
+            }
+        });
+
+        return dataMapping;
+    };
+
+    service.setValuesToChart = function (dataMapping, attributes, labels, data) {
+        for (var i = 0; i < labels.length; i++) {
+            if (typeof dataMapping[labels[i]] === 'undefined') {
+                angular.forEach(attributes, function (attribute, key) {
+                    data[key].push(0);
+                });
+            } else {
+                angular.forEach(attributes, function (attribute, key) {
+                    data[key].push(dataMapping[labels[i]][attribute]);
+                });
+            }
+        }
+    };
+
+    service.getDataForChart = function (data, attributes) {
+        //set date to usable format
+        service.setDataWithDate(data);
+
+        //map values to date
+        return service.getMapValuesToDate(data, attributes);
+    };
+
+}]);
--- src/resources/js/controllers/mainCtrl.js
+++ src/resources/js/controllers/mainCtrl.js
@@ -1,116 +1,73 @@
 /* use strict */
-app.controller('MainCtrl', ['$scope', 'foodDataFactory', 'poopDataFactory', '$q', function ($scope, foodDataFactory, poopDataFactory, $q) {
-    var ctrl = this;
+app.controller('MainCtrl', [
+    '$scope',
+    'foodDataFactory',
+    'poopDataFactory',
+    'chartService',
+    '$q',
+    function ($scope, foodDataFactory, poopDataFactory, chartService, $q) {
+        var ctrl = this;
 
-    $scope.series = [
-        'Food weight',
-        'Dufalact',
-        'Poop'
-    ];
-
-    ctrl.attributesFood = {
-        0: "weight",
-        1: "dufalact"
-    };
-
-    ctrl.attributesPoop = {
-        2: "size"
-    };
-
-    ctrl.dataMappingFood = {};
-    ctrl.dataMappingPoop = {};
-
-    $scope.$watch('days', function () {
-        $scope.labels = [];
-        $scope.data = [
-            [],
-            [],
-            []
+        $scope.series = [
+            'Food weight',
+            'Dufalact',
+            'Poop'
         ];
 
-        ctrl.generateDayRange($scope.days);
-        ctrl.setValuesToChart(ctrl.dataMappingFood, ctrl.attributesFood);
-        ctrl.setValuesToChart(ctrl.dataMappingPoop, ctrl.attributesPoop);
-    });
+        ctrl.attributesFood = {
+            0: "weight",
+            1: "dufalact"
+        };
 
-    ctrl.dateToYMD = function (date) {
-        var d = date.getDate();
-        var m = date.getMonth() + 1;
-        var y = date.getFullYear();
-        return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-    };
+        ctrl.attributesPoop = {
+            2: "size"
+        };
 
-    ctrl.generateDayRange = function (day) {
-        for (var i = 0; i < day; i++) {
-            var dateObj = new Date();
-            dateObj.setDate(dateObj.getDate() - i);
-            $scope.labels.push(ctrl.dateToYMD(dateObj));
-        }
-    };
+        ctrl.dataMappingFood = {};
+        ctrl.dataMappingPoop = {};
 
-    ctrl.setDataWithDate = function (data) {
-        angular.forEach(data, function (value) {
-            var date = value.datetime.date;
-            if (date !== undefined) {
-                value.datetime.date = date.split(" ")[0];
-            }
-        });
-    };
+        $scope.$watch('days', function () {
+            $scope.labels = [];
+            $scope.data = [
+                [],
+                [],
+                []
+            ];
 
-    ctrl.getMapValuesToDate = function (data, attributes) {
-        var dataMapping = {};
+            $scope.labels = chartService.getDayRange($scope.days);
 
-        angular.forEach(data, function (value) {
-            var date = value.datetime.date;
-            if (dataMapping[date] === undefined) {
-                dataMapping[date] = {};
-                angular.forEach(attributes, function (attribute) {
-                    dataMapping[date][attribute] = value[attribute];
-                });
-            } else {
-                angular.forEach(attributes, function (attribute) {
-                    dataMapping[date][attribute] += value[attribute];
-                });
-            }
+            chartService.setValuesToChart(
+                ctrl.dataMappingFood,
+                ctrl.attributesFood,
+                $scope.labels,
+                $scope.data
+            );
+            chartService.setValuesToChart(
+                ctrl.dataMappingPoop,
+                ctrl.attributesPoop,
+                $scope.labels,
+                $scope.data
+            );
         });
 
-        return dataMapping;
-    };
-
-    ctrl.setValuesToChart = function (dataMapping, attributes) {
-        for (var i = 0; i < $scope.labels.length; i++) {
-            if (typeof dataMapping[$scope.labels[i]] === 'undefined') {
-                angular.forEach(attributes, function (attribute, key) {
-                    $scope.data[key].push(0);
+        ctrl.getData = function () {
+            $q.all(
+                    [
+                        poopDataFactory.getAll(),
+                        foodDataFactory.getAll()
+                    ]
+                ).then(function (response) {
+                    ctrl.dataMappingPoop = chartService.getDataForChart(
+                        response[0].data.poops,
+                        ctrl.attributesPoop
+                    );
+                    ctrl.dataMappingFood = chartService.getDataForChart(
+                        response[1].data.foods,
+                        ctrl.attributesFood
+                    );
+                    $scope.days = 5;
                 });
-            } else {
-                angular.forEach(attributes, function (attribute, key) {
-                    $scope.data[key].push(dataMapping[$scope.labels[i]][attribute]);
-                });
-            }
-        }
-    };
+        };
 
-    ctrl.getDataForChart = function (data, attributes) {
-        //set date to usable format
-        ctrl.setDataWithDate(data);
-
-        //map values to date
-        return ctrl.getMapValuesToDate(data, attributes);
-    };
-
-    ctrl.getData = function () {
-        $q.all(
-                [
-                    poopDataFactory.getAll(),
-                    foodDataFactory.getAll()
-                ]
-            ).then(function (response) {
-                ctrl.dataMappingPoop = ctrl.getDataForChart(response[0].data.poops, ctrl.attributesPoop);
-                ctrl.dataMappingFood = ctrl.getDataForChart(response[1].data.foods, ctrl.attributesFood);
-                $scope.days = 5;
-            });
-    };
-
-    ctrl.getData();
-}]);
+        ctrl.getData();
+    }]);
